9c47e0c464b31dfe9df53c2d929eccc10d99b6fd
Davidm723 <davidmullokandov@gmail.com>
2025-12-10 19:58:49 -0500

    make authenticated requests

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
+import { useContext } from "react";
 import { Route, Routes } from "react-router";
 
 import NavBar from "./components/NavBar/NavBar";
 import SignUpForm from "./components/SignUpForm/SignUpForm";
 import SignInForm from "./components/SignInForm/SignInForm";
+import Landing from "./components/Landing/Landing";
+import Dashboard from "./components/Dashboard/Dashboard";
+
+import { UserContext } from "./contexts/UserContext";
 
 const App = () => {
+  const { user } = useContext(UserContext);
   return (
     <>
       <NavBar />
       <main className="main-container">
-      <Routes>
-        <Route path="/sign-up" element={<SignUpForm />} />
-        <Route path="/sign-in" element={<SignInForm />} />
-        <Route path="*" element={<h1>Nothing here</h1>} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={user ? <Dashboard /> : <Landing />} />
+          <Route path="/sign-up" element={<SignUpForm />} />
+          <Route path="/sign-in" element={<SignInForm />} />
+          <Route path="*" element={<h1>Nothing here</h1>} />
+        </Routes>
       </main>
     </>
   );
